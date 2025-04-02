ae00d0d4fd2af0694507c51d6c23cd6f080512d2
Marcelo Vansovski de Melo <marcelo_vansovski@hotmail.com>
2025-04-02 15:32:03 -0300

    Solucao

--- Desenvolvedor2/Pergunta05/Program.cs
+++ Desenvolvedor2/Pergunta05/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string texto = "DesafioDesenvolvedor02";
+
+        List<char> listaCaracteres = new List<char>(texto.ToCharArray());
+
+        var tamanho = texto.Length;
+
+       
+        Console.WriteLine("String Invertida:");
+        for (int i = tamanho - 1; i >= 0; i--)
+        {
+            Console.Write(listaCaracteres[i]);
+        }
+    }
+}
