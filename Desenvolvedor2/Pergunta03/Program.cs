91d346e3935ccb4f99408c92527b93f10b954b03
Marcelo Vansovski de Melo <marcelo_vansovski@hotmail.com>
2025-04-02 15:10:36 -0300

    Numero de dias com faturamento superior a média

--- Desenvolvedor2/Pergunta03/Program.cs
+++ Desenvolvedor2/Pergunta03/Program.cs
@@ -24,12 +24,24 @@ class Program
             var menorFaturamento = listFaturamento?.OrderBy(x => x.Valor).First(); 
             var maiorFaturamento = listFaturamento?.OrderByDescending(x => x.Valor).First();
 
-            
+            var mediaFaturamento = listFaturamento?.Average(x => x.Valor);  
 
+            int numDiasFaturamentoMaior = 0;    
+
+            listFaturamento.ForEach(faturamento =>
+            {
+                if (faturamento.Valor > mediaFaturamento)
+                {
+                    numDiasFaturamentoMaior++;
+                }
+            });
 
 
             Console.WriteLine($"O menor valor de faturamento foi: {menorFaturamento?.Valor} no dia {menorFaturamento?.Dia}");
             Console.WriteLine($"O maior valor de faturamento foi: {maiorFaturamento?.Valor} no dia {maiorFaturamento?.Dia}");
+        
+
+            Console.WriteLine($"Número de dias com faturamento maior que a média: {numDiasFaturamentoMaior}");
         }
         catch (Exception ex)
         {
