3b1a2cf7c5e32b18f6f7cdd5e091d29df5a4bab2
Marcelo Vansovski de Melo <marcelo_vansovski@hotmail.com>
2025-04-02 14:41:48 -0300

    Função que valida fibonnacci

--- Desenvolvedor2/Pergunta02/Program.cs
+++ Desenvolvedor2/Pergunta02/Program.cs
@@ -1,4 +1,42 @@
-﻿Console.WriteLine("Digite um número:");
+﻿using System.Security;
+
+Console.WriteLine("Digite um número:");
 
 // Lê a entrada do usuário como uma string
 string possivelFibo = Console.ReadLine();
+
+if (int.TryParse(possivelFibo, out int numero))
+{
+
+
+
+
+}
+else
+{
+    Console.WriteLine("O valor digitado não é um número válido.");
+}
+
+
+bool Fibonacci(int numero)
+{
+    int a = 0;
+    int b = 1;
+
+   
+    if (numero == 0 || numero == 1)
+        return true;
+
+    while (b <= numero)
+    {
+        int soma = a + b;
+
+        if (soma == numero)
+            return true;
+
+        a = b;
+        b = soma;
+    }
+
+    return false;
+}
