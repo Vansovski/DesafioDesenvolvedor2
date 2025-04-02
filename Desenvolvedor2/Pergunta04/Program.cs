ab4f2f53e08427b6be4581ebeba7da46bb768691
Marcelo Vansovski de Melo <marcelo_vansovski@hotmail.com>
2025-04-02 15:18:23 -0300

    Entrada dos valores hardcode

--- Desenvolvedor2/Pergunta04/Program.cs
+++ Desenvolvedor2/Pergunta04/Program.cs
@@ -1,7 +1,25 @@
-﻿public class FaturamentoUF
+﻿using System;
+using System.Collections.Generic;
+
+public class FaturamentoUF
 {
-    public int Uf { get; set; }
+    public string? Uf { get; set; }
     public double Valor { get; set; }
 }
 
+class Program
+{
+    static void Main()
+    {
+        List<FaturamentoUF> listFaturamentoUF = new List<FaturamentoUF>();
 
+        listFaturamentoUF.Add(new FaturamentoUF { Uf = "SP", Valor = 67836.43 });
+        listFaturamentoUF.Add(new FaturamentoUF { Uf = "RJ", Valor = 36678.66 });
+        listFaturamentoUF.Add(new FaturamentoUF { Uf = "MG", Valor = 29229.88 });
+        listFaturamentoUF.Add(new FaturamentoUF { Uf = "ES", Valor = 27165.48 });
+        listFaturamentoUF.Add(new FaturamentoUF { Uf = "Outros", Valor = 19849.53 });
+    
+       
+    
+    }
+}
