de729f66463d6643081e1e89ef9a2bb6a41e08cd
Marcelo Vansovski de Melo <marcelo_vansovski@hotmail.com>
2025-04-02 14:59:58 -0300

    Menor Faturamento

--- Desenvolvedor2/Pergunta03/Program.cs
+++ Desenvolvedor2/Pergunta03/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+
+class Program
+{
+    // Classe para representar cada item do JSON
+    public class Faturamento
+    {
+        public int Dia { get; set; }
+        public double Valor { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+      
+        try
+        {
+
+            string caminhoArquivo = Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName, "dados.json");
+
+            string json = File.ReadAllText(caminhoArquivo);
+
+            var listFaturamento = JsonConvert.DeserializeObject<List<Faturamento>>(json);
+
+            var menorFaturamento = listFaturamento?.Min(d => d.Valor);
+
+            Console.WriteLine($"O menor valor de faturamento foi: {menorFaturamento}");
+
+
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao ler o arquivo JSON: {ex.Message}");
+        }
+    }
+}
